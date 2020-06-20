f598a048993d4cbfc220cdc1327d7c545118fe96
Maksym <gontcharuk@gmail.com>
2020-06-20 13:09:43 +0200

    Добавил компонент кнопки

--- src/components/FinishedQuiz/FinishedQuiz.jsx
+++ src/components/FinishedQuiz/FinishedQuiz.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../UI/Button/Button';
 import classes from './FinishedQuiz.module.css';
 
 const FinishedQuiz = props => {
@@ -30,7 +31,13 @@ const FinishedQuiz = props => {
             <p>Правильно { countSuccess } из { props.quiz.length }</p>
 
             <div>
-                <button onClick={ props.onRetry }>Повторить</button>
+                <Button
+                    onClick={ props.onRetry }
+                    type="primary"
+                >Повторить</Button>
+                <Button
+                    type="success"
+                >Перейти в список тестов</Button>
             </div>
         </div>
     )
